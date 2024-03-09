94b563590345f671660eb2d3c18cf4ca1cab05de
Gilson Fonseca <gf.gilsonfonseca@gmail.com>
2024-03-09 04:58:16 -0300

    feat: configurando FluentValidation

--- GerenciadorTurmas.Api/Program.cs
+++ GerenciadorTurmas.Api/Program.cs
@@ -1,24 +1,25 @@
 using FluentValidation;
-using GerenciadorTurmas.Api.Common;
 using GerenciadorTurmas.Api.Models.Aluno;
 using GerenciadorTurmas.Application.UseCases;
 using GerenciadorTurmas.Domain.Contracts.Repositories.Aluno;
 using GerenciadorTurmas.Domain.Contracts.UseCases.Aluno;
 using GerenciadorTurmas.Infrastructure.DbContext;
 using GerenciadorTurmas.Infrastructure.Repositories.Aluno;
-using Microsoft.AspNetCore.SignalR;
-using System.Globalization;
-using AutoMapper;
-using Microsoft.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IDbContext, DbContext>();
+
 builder.Services.AddScoped<IAlunoRepository, AlunoRepository>();
+
 builder.Services.AddScoped<IAlunoUseCase, AlunoUseCase>();
-builder.Services.AddScoped<IValidator, AlunoInputValidator>();
 
 builder.Services.AddAutoMapper(typeof(AlunoMapProfile));
+
+builder.Services.AddFluentValidationAutoValidation();
+
+builder.Services.AddValidatorsFromAssemblyContaining<AlunoInputValidator>();
 
 builder.Services.AddControllers();
 
@@ -34,8 +35,6 @@ if (app.Environment.IsDevelopment())
 
     app.UseSwaggerUI();
 }
-
-app.UseMiddleware<ValidationMiddleware>();
 
 app.UseHttpsRedirection();
 
